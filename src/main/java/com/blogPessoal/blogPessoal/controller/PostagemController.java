562deb72b35a442da8e76ad140ecf737f527e942
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-02-18 11:54:53 -0300

    Metodos Post e Put

--- src/main/java/com/blogPessoal/blogPessoal/controller/PostagemController.java
+++ src/main/java/com/blogPessoal/blogPessoal/controller/PostagemController.java
@@ -3,10 +3,16 @@ package com.blogPessoal.blogPessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +21,7 @@ import com.blogPessoal.blogPessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin("*")//aceitar requisições de qlqr origin
+@CrossOrigin("*")//aceitar requisições de qlqr origem
 public class PostagemController {
 
 	@Autowired
@@ -37,5 +43,21 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){//@RequestBody - Para Recepcionar os valores/objetos que são passadas via body para nossa aplicação
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
 	
 }
